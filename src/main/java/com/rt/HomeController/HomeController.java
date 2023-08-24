96575d2768e33df60dbcf220ff98db4a96afb02f
AkshayGade1229 <akshaygade1229@gmail.com>
2023-08-24 12:23:06 +0530

    Change in HomeController and Services

--- src/main/java/com/rt/HomeController/HomeController.java
+++ src/main/java/com/rt/HomeController/HomeController.java
@@ -1,6 +1,11 @@
 package com.rt.HomeController;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +28,32 @@ public class HomeController {
 
 	}
 	
+	//-----------------------------	update---------------------------------------------------------------------------
+	
+		@PostMapping("/update")
+		public String update(@RequestBody Entityes e) {
+		
+			s.update(e);
+			
+			return"Emp update Successfully....!";
+
+		}
+	//-----------------------------------Get Emp BY Id------------------------------------------------------------------
+		@GetMapping("/id/{id}")
+		public Optional<Entityes> findById(@PathVariable int id) {
+		
+			Optional<Entityes> e1 = s.findById(id);
+			
+			return e1;
+
+		}
+	//----------------------------------Delete Emp ---------------------------------------------------------------------------
+		@DeleteMapping("/id/{id}")
+		public String deleteById(@PathVariable Entityes id) {
+		
+			Entityes e2 = s.deleteById(id);
+			
+			return "Employee Deleted successfully....!";
+
+		}
 }
--- src/main/java/com/rt/Services/Service.java
+++ src/main/java/com/rt/Services/Service.java
@@ -1,6 +1,8 @@
 package com.rt.Services;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +19,28 @@ public class Service {
 		
 		Dao.save(e);
 		
+	}
+
+	public void update(Entityes e) {
+		Dao.save(e);
+		
+	}
+
+	public Optional<Entityes> findById(int id) {
+		
+		Optional<Entityes> e2 = Dao.findById(id);
+		
+		if (e2.isPresent()) {
+			e2.get();
+			return e2;
+
+		}
+		return null;
+	}
+
+	public Entityes deleteById(Entityes id) {
+		Dao.delete(id);
+		return null;
 	}
 
 }
