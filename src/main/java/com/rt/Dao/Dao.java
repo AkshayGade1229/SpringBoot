c0f99b5ec46f1381842eba01925b3c1067e276e4
AkshayGade1229 <akshaygade1229@gmail.com>
2023-08-28 09:11:42 +0530

    Using JPQL Query FindByName

--- src/main/java/com/rt/Dao/Dao.java
+++ src/main/java/com/rt/Dao/Dao.java
@@ -4,12 +4,24 @@ package com.rt.Dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.rt.Entityes.Entityes;
 
 public interface Dao extends JpaRepository<Entityes, Integer>{
 
-	List<Entityes> findByName(String name);
+	
+	
+	 List<Entityes> findByName(String name);
+	
+	//@Query(value= "select * from spring_boot where name = ?",nativeQuery = true )
+	//List<Entityes> xyz (String name);
+
+		
+		// JPQL
+		@Query(value= "select e from Entityes e where e.name=:n", nativeQuery = false)
+	List<Entityes> xyz (@Param("n") String n);
 
 }
 
